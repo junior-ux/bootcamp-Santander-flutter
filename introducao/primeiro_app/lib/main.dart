e68ef9d370d835de695303415335ad87d28c2684
junior-ux <junniorlima90@gmail.com>
2025-03-23 22:41:08 -0300

    fundamentos da linguagem dart

--- introducao/primeiro_app/lib/main.dart
+++ introducao/primeiro_app/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -38,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.onPrimary,
