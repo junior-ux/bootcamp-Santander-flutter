30aace4aa3dad5941f38fb205bd6477e4afb7e29
junior-ux <junniorlima90@gmail.com>
2025-04-05 18:13:11 -0300

    tipos de váriáveis e listas

--- introducao/fundamentos/fundamentos/bin/fundamentos.dart
+++ introducao/fundamentos/fundamentos/bin/fundamentos.dart
@@ -1,5 +1,21 @@
 import 'package:fundamentos/fundamentos.dart' as fundamentos;
 
 void main(List<String> arguments) {
-  print('Hello world: ${fundamentos.calculate()}!');
+  String texto = "Olá";
+  var texto2 = "Bootcamp";
+  String texto3;
+
+  List<String> lista = [];
+  var lista1 = [];
+
+  lista.add("a");
+  lista.add("Narciso");
+  lista.add("Junior");
+  lista1.add(1);
+  lista1.add(texto2);
+  lista1.add(texto);
+  lista1.add(25);
+
+  print(lista1);
+  print(lista);
 }
