10ae3f4c6dfbeebd4f1690f39703eca89c1a3d32
junior-ux <junniorlima90@gmail.com>
2025-04-12 20:45:51 -0300

    estrutura condicional ternaria

---
+++ introducao/fundamentos/fundamentos/bin/estruturas_condicionais/condicao_if_ternario.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  var prova1 = 8;
+  var prova2 = 2;
+  var media = (prova1 + prova2) / 2;
+  print("A média é: $media");
+  // Operador ternário
+  var resultado = media >= 7 ? "Aprovado" : "Reprovado";
+  print(resultado);
+}
